b86742de089b710d6d24ff146bd64370bcba7ac6
vitaemetus <bazaliy.daniil@gmail.com>
2022-11-15 13:27:35 +0300

    all done!

--- lab8/1_task.py
+++ lab8/1_task.py
@@ -1,35 +1,31 @@
 import pygame
-import math
-from pygame.draw import *
+from pygame.draw import circle
+from pygame.draw import polygon
 
 pygame.init()
 
-FPS = 1
+
+FPS = 30
 screen = pygame.display.set_mode((400, 400))
+screen.fill((125, 125, 125))
 
-# rgb-кодировки:
-red = (255, 0, 0)
-yellow = (255, 255, 0)
-black = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (125, 125, 125)
+LIGHT_BLUE = (64, 128, 255)
+GREEN = (0, 200, 64)
+YELLOW = (225, 225, 0)
+PINK = (230, 50, 230)
 
-# координаты окружностей:
-c0 = (200, 200)
-cl = (160, 175)
-cr = (240, 175)
-
-# рисование:
-circle(screen, yellow, c0, 100, 0)
-
-circle(screen, red, cl, 20, 0)
-circle(screen, black, cl, 10, 0)
-
-circle(screen, red, cr, 15, 0)
-circle(screen, black, cr, 7, 0)
-# координаты "бровей":
-phi = 3.14 / 2  # угол наклона
-
-rect(screen, black, (150, 250, 100, 20), 0)
-
+pygame.draw.circle(screen, YELLOW, (200, 200), 145)
+pygame.draw.circle(screen, BLACK, (200, 200), 150, 5)
+pygame.draw.circle(screen, PINK, (140, 160), 25, 10)
+pygame.draw.circle(screen, PINK, (260, 160), 25, 15)
+pygame.draw.circle(screen, BLACK, (140, 160), 15)
+pygame.draw.circle(screen, BLACK, (260, 160), 10)
+pygame.draw.polygon(screen, LIGHT_BLUE, [[140, 270], [260, 270], [260, 280], [140, 280]])
+pygame.draw.polygon(screen, BLACK, [[180, 160], [180, 150], [100, 90], [100, 100]])
+pygame.draw.polygon(screen, BLACK, [[220, 160], [220, 150], [310, 80], [320, 80]])
 
 
 pygame.display.update()
