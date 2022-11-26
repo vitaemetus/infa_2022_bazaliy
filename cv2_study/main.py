14ea10a01ef3e8799d5f11cb818195eefa2adef6
vitaemetus <bazaliy.daniil@gmail.com>
2022-11-26 20:36:07 +0300

    comments added

--- cv2_study/main.py
+++ cv2_study/main.py
@@ -5,30 +5,15 @@ import mss
 import win32gui
 import pyautogui
 
-# img = cv2.imread('images/ust.jpg')
-# cv2.imshow('Output', img)
-#
-# cv2.waitKey(0)
-
-
-# cap = cv2.VideoCapture('videos/hurt.mp4')
-#
-# while True:
-#     success, img = cap.read()
-#     cv2.imshow('Output', img)
-#
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-
-# cap = cv2.VideoCapture("videos/hurt.mp4")
+# маска для сглаживания входящей картинки:
 kernel = np.ones((5, 5), 'uint8')
 
 while True:
-    window_rect = (120, 650, 220, 220)
-    img = pyautogui.screenshot(region=window_rect)
-    img = np.array(img)
-    erode_img = cv2.erode(img, kernel, iterations=1)
-    (thresh, img) = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
+    window_rect = (120, 650, 220, 220)  # область работыOpenCV
+    img = pyautogui.screenshot(region=window_rect)  # CV делает скриншот своей области
+    img = np.array(img)  # конвертация скриншота в список
+    erode_img = cv2.erode(img, kernel, iterations=1)  # сглаживание
+    (thresh, img) = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)  # отсеивание пикселей
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     cv2.imshow('Output', img)
