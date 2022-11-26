273e3230d14c09703b85648a8deaaaf20973a7e6
vitaemetus <bazaliy.daniil@gmail.com>
2022-11-26 20:29:56 +0300

    start

--- cv2_study/main.py
+++ cv2_study/main.py
@@ -1,8 +1,9 @@
 import cv2
-import numpy
-
+import numpy as np
+from PIL import Image
 import mss
-
+import win32gui
+import pyautogui
 
 # img = cv2.imread('images/ust.jpg')
 # cv2.imshow('Output', img)
@@ -19,20 +20,18 @@ import mss
 #     if cv2.waitKey(1) & 0xFF == ord('q'):
 #         break
 
-
-import numpy as np
-import cv2
-from mss import mss
-from PIL import Image
-
-bounding_box = {'top': 100, 'left': 0, 'width': 400, 'height': 300}
-
-sct = mss()
+# cap = cv2.VideoCapture("videos/hurt.mp4")
+kernel = np.ones((5, 5), 'uint8')
 
 while True:
-    sct_img = sct.grab(bounding_box)
-    cv2.imshow('screen', np.array(sct_img))
+    window_rect = (120, 650, 220, 220)
+    img = pyautogui.screenshot(region=window_rect)
+    img = np.array(img)
+    erode_img = cv2.erode(img, kernel, iterations=1)
+    (thresh, img) = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    if (cv2.waitKey(1) & 0xFF) == ord('q'):
-        cv2.destroyAllWindows()
+    cv2.imshow('Output', img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
